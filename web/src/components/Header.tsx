bd72598e5e6a8778c91599a40b77590d248a7a59
ronak-pal1 <ronakpaul882@gmail.com>
2025-10-14 10:01:45 +0530

    Responsiveness of web

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,15 +1,29 @@
 import { LiaArrowRightSolid } from "react-icons/lia";
 import { CiStar } from "react-icons/ci";
 import { Link } from "react-router-dom";
+import lightLogo from "../assets/light.png";
+import { FaGithub } from "react-icons/fa";
 
 const Header = () => {
   return (
     <header className="bg-transparent flex justify-between items-center sticky top-0 z-50 px-8 py-7 backdrop-blur-sm">
-      <div>
+      <div className="flex items-center gap-4 cursor-pointer">
+        <img src={lightLogo} alt="Logo" className="w-[35px] object-contain"/>
         <h1 className="text-xl text-white">Create Tall</h1>
       </div>
       <div className="flex items-center gap-5">
-        <Link
+
+        {
+          window.innerWidth < 1024 ? (
+            <Link
+          to="https://github.com/ronak-pal1/create-tall"
+          target="_blank"
+          className="text-white bg-black border border-neutral-700 shadow-2xl shadow-blue-300 px-2 py-2 rounded-full cursor-pointer hover:scale-105 transition-all font-light flex items-center gap-2"
+        >
+          <FaGithub />
+        </Link>
+          ):(
+<Link
           to="https://github.com/ronak-pal1/create-tall"
           target="_blank"
           className="text-white bg-black border border-neutral-700 shadow-2xl shadow-blue-300 px-5 py-1 text-sm rounded-full cursor-pointer hover:scale-105 transition-all font-light flex items-center gap-2"
@@ -17,6 +31,9 @@ const Header = () => {
           <p>Star on GitHub</p>
           <CiStar />
         </Link>
+          )
+        }
+        
         <Link
           to="https://tall.docs.ronakpaul.com"
           target="_blank"
--- web/src/components/HeroSection.tsx
+++ web/src/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 import { GoChevronRight } from "react-icons/go";
 import { MdOutlineCopyAll } from "react-icons/md";
-import {GetRequiredSvg} from "./svg";
-import terminalImage from "../assets/terminal.png"
+import { GetRequiredSvg } from "./svg";
+import terminalImage from "../assets/terminal.png";
 
 const HeroSection = () => {
   const handleCopy = () => {
@@ -9,57 +9,65 @@ const HeroSection = () => {
   };
 
   return (
-    <section className="h-screen w-full relative">
-      <div className="text-white w-full text-center flex flex-col justify-center items-center mt-10 gap-y-20">
-        <div className="text-center space-y-7">
-          <h2 className="text-3xl font-bold">Create Tall CLI</h2>
-          <p className="text-lg font-light">
+    <section className="h-[80vh] md:h-screen w-full relative">
+      <div className="text-white w-full text-center flex flex-col justify-center items-center mt-36 lg:mt-10 gap-y-20">
+        <div className="text-center space-y-7 px-14 z-10">
+          <h2 className="text-2xl sm:text-3xl font-bold">Create Tall CLI</h2>
+          <p className="text-sm sm:text-lg font-light">
             A powerful tool to create any stack applications with ease and all
             setup ready
           </p>
         </div>
 
-        <div className="w-[400px] shadowed-div flex justify-between items-center bg-black py-4 px-5 rounded-xl">
+        <div className="w-[75%] sm:w-[400px] shadowed-div flex justify-between items-center bg-black py-4 px-5 rounded-xl z-10">
           <div className="flex items-center gap-2">
             <GoChevronRight className="text-amber-500" />
             <p className=" text-sm text-neutral-300">npx create-tall</p>
           </div>
 
           <div>
-            <MdOutlineCopyAll onClick={handleCopy} className="cursor-pointer hover:text-amber-500"/>
+            <MdOutlineCopyAll
+              onClick={handleCopy}
+              className="cursor-pointer hover:text-amber-500"
+            />
           </div>
         </div>
       </div>
 
-      <div className="absolute bottom-0  w-full  flex justify-center">
-        <div className="h-[500px] w-[70%] bg-black rounded-2xl shadowed-div">
-        <img src={terminalImage} alt="Terminal Image" width="100%" height="100%" className="object-cover"/>
+      <div className="absolute bottom-0  w-full  flex justify-center z-10">
+        <div className="h-[250px] sm:h-[300px] lg:h-[500px] w-[85%] lg:w-[70%] bg-black rounded-2xl shadowed-div">
+          <img
+            src={terminalImage}
+            alt="Terminal Image"
+            width="100%"
+            height="100%"
+            className="object-cover"
+          />
         </div>
       </div>
 
-
-      <div  className="absolute right-20 top-40 rotate-12 opacity-45">
-        <GetRequiredSvg logoName="tailwindcss" size={100} />
+      <div className="absolute right-20 top-36 sm:top-40 rotate-12 opacity-45 z-0">
+        <GetRequiredSvg logoName="tailwindcss" size={window.innerWidth < 1024 ? 60 : 100} />
       </div>
 
-      <div  className="absolute left-10 top-30 rotate-12 opacity-45">
-        <GetRequiredSvg logoName="reactjs" size={100} />
+      <div className="absolute left-10 top-30 rotate-12 opacity-45 z-0">
+        <GetRequiredSvg logoName="reactjs" size={window.innerWidth < 1024 ? 60 : 100} />
       </div>
 
-      <div  className="absolute left-14 top-[350px] rotate-12 opacity-45">
-        <GetRequiredSvg logoName="nextjs" size={70} />
+      <div className="absolute left-14 top-[350px] rotate-12 opacity-45 z-0">
+        <GetRequiredSvg logoName="nextjs" size={window.innerWidth < 1024 ? 60 : 70} />
       </div>
 
-      <div  className="absolute left-20 top-[500px] -rotate-12 opacity-45">
-        <GetRequiredSvg logoName="redux" size={80} />
+      <div className="absolute left-36 top-[300px] md:top-[500px] -rotate-12 opacity-45 z-0">
+        <GetRequiredSvg logoName="redux" size={window.innerWidth < 1024 ? 60 : 80} />
       </div>
 
-      <div  className="absolute right-20 top-[500px] rotate-12 opacity-45">
-        <GetRequiredSvg logoName="expressjs" size={80} color="#61DAFB"/>
+      <div className="absolute right-40 top-[300px] md:top-[500px] rotate-12 opacity-45 z-0">
+        <GetRequiredSvg logoName="expressjs" size={window.innerWidth < 1024 ? 60 : 80} color="#61DAFB" />
       </div>
 
-      <div  className="absolute right-14 top-[350px] rotate-12 opacity-45">
-        <GetRequiredSvg logoName="go" size={80} color="#61DAFB"/>
+      <div className="absolute right-14 top-[350px] rotate-12 opacity-45 z-0">
+        <GetRequiredSvg logoName="go" size={window.innerWidth < 1024 ? 60 : 80} color="#61DAFB" />
       </div>
     </section>
   );
